e364bb9c31e95a36b06d0786ae168b3dfda6904a
Keerthana264 <keerthanahoney25@gmail.com>
2020-07-20 13:33:34 +0530

    Commit message

---
+++ src/main/java/tddkeerthana/testcases_demo.java
@@ -0,0 +1,18 @@
+package tddkeerthana;
+
+public class testcases_demo {
+	public String valid_string(String str) {
+		String m="";
+		for(int i=0;i<2;i++) {
+			if(str.charAt(i)!='A') {
+				m=m+str.charAt(i);
+				
+			
+			}
+		}
+		m=m+str.substring(2);
+		return m;
+		
+	}
+
+}
